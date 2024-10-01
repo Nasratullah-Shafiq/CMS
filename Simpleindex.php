851691bfa69fa1da2caba9da092242a9a4ff81a5
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-10-01 21:42:43 +0430

    Adding models

---
+++ Simpleindex.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Change English Center </title>
+  <meta charset="utf-16">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <link href = "Assets/img/icons/Training_96px.png" rel="icon" type="image/png" >
+    <link href = "./Assets/CSS/bootstrap.css" rel="stylesheet" type = "text/css">
+    <link href = "./Assets/CSS/bootstrap.min.css" rel="stylesheet" type = "text/css">
+    <!-- <link href = "./Assets/CSS/Change_Admin_Panel" rel="stylesheet" type = "text/css"> -->
+    
+
+    <link href = "./Assets/CSS/font-awesome.css" rel="stylesheet" type = "text/css">
+    <link href = "./Assets/CSS/font-awesome.min.css" rel="stylesheet" type = "text/css">
+    <link href = "./Assets/CSS/Change_Course_Style.css" rel="stylesheet" type = "text/css">
+    <link href = "./Assets/CSS/carousel.css" rel="stylesheet" type = "text/css">
+    <link href = "./Assets/CSS/bootstrap.min.css" rel="stylesheet" type = "text/css">  
+
+    <link href = "./Assets/js/owl.carousel.min.css" rel = "Stylesheet" type = "text/css"/>
+    
+    <!-- <link href = "./Assets/CSS/owl.theme.default.min.css" rel = "Stylesheet" type = "text/css"/> -->
+    <link href = "./Assets/CSS/docs.the.min.css" rel = "Stylesheet" type = "text/css"/>
+    
+    <script type = "text/javascript" src = "./Assets/js/jquery.js"></script>
+    <script type = "text/javascript" src = "./Assets/js/bootstrap.js"></script>
+    <script type = "text/javascript" src = "./Assets/js/popper.min.js"></script>
+    <script type = "text/javascript" src = "./Assets/js/bootstrap.min.js"></script>
+    
+    
+    
+    <script type = "text/javascript" src = "./Assets/js/Change.js"></script>
+    <script type = "text/javascript" src = "./Assets/js/AjaxSearch.js"></script>
+
+    
+     <!--  <script type="text/javascript">
+      $(function () {
+        $('.mobile-nav').click(function() {
+          $('.nav-left-side').toggleClass('visible'); 
+        })
+      })
+    </script>
+ -->
+    <!-- <script type="text/javascript">
+            $(document).ready(function(){
+                $(function(){
+                    $(this).bind("contextmenu", function(event){
+                        event.preventDefault();
+                        // alert("Right click is disabled due to security issue");
+                    });
+                });
+            });
+        </script>  -->
+</head>
+<body>
+    
+    <script type = " text/javascript" src="./Assets/Owl-js/jquery.min.js"></script>
+    <script type = " text/javascript" src="./Assets/Owl-js/owl.carousel.js"></script> 
+
+        <!-- End of Container -->
+        <div class="ro">
+        <div class="container">
+    
+          <h2 class="padding-top: 10px; padding-bottom: 20px; margin-bottom: 20px;"> SEMESTERS </h2>
+          <section class="testimonials-section">
+              <div class="container-fluid px-lg-4 px-md-4">
+                <div class="testimonials">
+                    <div id="carousel1" class="owl-carousel">
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/startup.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3>START-UP </h3>
+                                    
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/openup.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3>OPEN-UP </h3>
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/warmup.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3>WARM-UP </h3>
+                                </div>
+                           </div>
+                       </div> 
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/runup.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3>RUN-UP </h3>
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/roundone.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3> ROUND ONE </h3>
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/roundtwo.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3>ROUND TWO </h3>
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/roundsix.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3> ROUND SIX </h3>
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/poineer.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3> POINEER </h3>
+                                </div>
+                           </div>
+                       </div>
+                       <div class="item">
+                           <div class="card text-center">
+                                <img class="img-circle" style = "border-radius: 5px;" src="Assets/img/Book/poineerplus.jpg" alt="Generic placeholder image" width="140" height="360">                                    
+                                <div class="card-body">
+                                    <h3> POINEER PLUS</h3>
+                                </div>
+                           </div>
+                       </div>
+
+                    </div>
+                </div>                  
+              </div>           
+          </section>
+        </div>            
+    </div>
+    <!-- End of Row Semester -->
+         
+    <div id = "topBtn"><i class = "fa fa-arrow-up" ></i></div>
+<script>
+            $(document).ready(function() {
+              $("#carousel1").owlCarousel({
+                    loop: true,
+                    center: true,
+                    margin: 10,
+                    responsiveClass: true,
+                    nav: true,
+                    responsive:{
+                        0:{
+                            items: 1
+                        },
+                        600:{
+                            items: 3
+                        },
+                        1000:{
+                            items: 4
+                        }
+                    }
+                });
+            });
+          </script>
+<script type="text/javascript">
+    
+            // $(document).ready(function() {
+            //   var owl = $('.owl-carousel');
+            //   owl.owlCarousel({
+            //     margin: 10,
+            //     nav: true,
+            //     loop: true,
+            //     responsive: {
+            //       0: {
+            //         items: 1
+            //       },
+            //       600: {
+            //         items: 3
+            //       },
+            //       1000: {
+            //         items: 4
+            //       }
+            //     }
+            //   })
+            // })
+          
+</script>
+    <script>
+  $(document).ready(function(){
+	$(window).scroll(function(){
+           if($(this).scrollTop()>100){
+            $('#topBtn').fadeIn();
+           }
+           else{
+             $('#topBtn').fadeOut();
+           } 
+        });
+        $("#topBtn").click(function(){
+            $('html, body').animate({scrollTop:0}, 1000);
+        });
+     });
+
+
+
+var firefoxTestDone = false
+function reportFirefoxTestResult(result) {
+  if (!firefoxTestDone) {
+    $('#ff-bug-test-result')
+      .addClass(result ? 'text-success' : 'text-danger')
+      .text(result ? 'PASS' : 'FAIL')  
+  }
+  firefoxTestDone = true
+}
+
+$(function () {
+  $('.js-popover').popover()
+  $('.js-tooltip').tooltip()
+  $('#tall-toggle').click(function () {
+    $('#tall').toggle()
+  })
+  $('#ff-bug-input').one('focus', function () {
+    $('#myModal2').on('focus', function () {
+      reportFirefoxTestResult(false)
+    })
+    $('#ff-bug-input').on('focus', function () {
+      reportFirefoxTestResult(true)
+    })
+  })
+})
+</script>
+
+
+<?php
+include('./Assets/_Partial Components/Footer.php');
+?>
